7c5dfe99d0aed0688ce033e80da8c605a6b54589
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-03-29 01:50:59 +0300

    [GOHW-4]: fix issue orders storage

--- internal/order/storage/database/issue_orders.go
+++ internal/order/storage/database/issue_orders.go
@@ -13,11 +13,11 @@ func (s *OrderStoragePG) IssueOrders(ctx context.Context, orderIDs map[uint64]st
 	args = append(args, true)
 	args = append(args, issueTime)
 	placeholders := make([]string, 0, len(orderIDs))
-	for i := 0; i < len(orderIDs); i++ {
-		placeholders = append(placeholders, fmt.Sprintf("$%d", i+3))
-	}
+	idCount := 0
 	for id := range orderIDs {
+		placeholders = append(placeholders, fmt.Sprintf("$%d", idCount+3))
 		args = append(args, id)
+		idCount++
 	}
 	query := fmt.Sprintf("UPDATE orders SET is_issued = $1, order_issue_date = $2 WHERE id IN (%s)", strings.Join(placeholders, ", "))
 	_, err := s.db.Cluster.Exec(ctx, query, args...)
