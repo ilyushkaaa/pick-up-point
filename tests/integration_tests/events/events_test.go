74ee4a0a6f43cf6a9ab6acdb9a546e711ccb2f91
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-04-12 03:00:40 +0300

    [GOHW-6]: add one more integration test

--- tests/integration_tests/events/events_test.go
+++ tests/integration_tests/events/events_test.go
@@ -15,26 +15,43 @@ import (
 )
 
 func TestLoggingEvents(t *testing.T) {
+	s := setUp(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer tearDown(t, cancel, s.syncProducer)
+	go func() {
+		err := consumer.Run(s.brokers, s.logger, ctx, "test_events", "test_group")
+		assert.NoError(t, err)
+	}()
 
-	t.Run("ok", func(t *testing.T) {
-		s := setUp(t)
-		ctx, cancel := context.WithCancel(context.Background())
-		defer tearDown(t, cancel, s.syncProducer)
-
-		go func() {
-			err := consumer.Run(s.brokers, s.logger, ctx, "test_events", "test_group")
-			assert.NoError(t, err)
-		}()
-
+	t.Run("test get", func(t *testing.T) {
 		req := httptest.NewRequest("GET", "http://127.0.0.1/pick-up-points", nil)
 		recorder := httptest.NewRecorder()
 		time.Sleep(time.Second * 7)
+
 		s.mw.AccessLog(&fakeHandler{}).ServeHTTP(recorder, req)
 		time.Sleep(time.Second)
+
 		fmt.Println(s.buf.String())
 		assert.Contains(t, s.buf.String(), "New request")
 		assert.Contains(t, s.buf.String(), `"method": "GET"`)
 		assert.Contains(t, s.buf.String(), `"url": "/pick-up-points"`)
+
+	})
+
+	t.Run("test post", func(t *testing.T) {
+		s.buf.Reset()
+		req := httptest.NewRequest("POST", "http://127.0.0.1/pick-up-point", nil)
+		recorder := httptest.NewRecorder()
+		time.Sleep(time.Second * 7)
+
+		s.mw.AccessLog(&fakeHandler{}).ServeHTTP(recorder, req)
+		time.Sleep(time.Second)
+
+		fmt.Println(s.buf.String())
+		assert.Contains(t, s.buf.String(), "New request")
+		assert.Contains(t, s.buf.String(), `"method": "POST"`)
+		assert.Contains(t, s.buf.String(), `"url": "/pick-up-point"`)
+
 	})
 
 }
