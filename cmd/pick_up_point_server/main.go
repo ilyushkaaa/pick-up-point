c07d355db6b65de77da784160671c83197e6d937
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-04-12 17:12:33 +0300

    [GOHW-6]: change one log

--- cmd/pick_up_point_server/main.go
+++ cmd/pick_up_point_server/main.go
@@ -26,7 +26,7 @@ import (
 func main() {
 	zapLogger, err := zap.NewProduction()
 	if err != nil {
-		log.Fatalf("error in events initialization: %v", err)
+		log.Fatalf("error in logger initialization: %v", err)
 	}
 	logger := zapLogger.Sugar()
 	defer func() {
